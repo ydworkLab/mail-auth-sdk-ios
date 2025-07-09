b892ceb4e5a85506ced826763d29b7e0f5191637
ydworkLab <service@youdo.com>
2025-07-09 21:32:04 +0300

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MRMailSDK",
+    defaultLocalization: "ru",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "MRMailSDK",
+            targets: ["MRMailSDK"]
+        ),
+    ],
+    targets: [
+        .target(
+            name: "MRMailSDK",
+            path: "mr-mail-sdk/sources"
+        )
+    ]
+)
